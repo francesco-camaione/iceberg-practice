d562310407d7f403abe8a6bfc1c783d93056228b
Francesco Camaione <france.cama@MBA-Francesco.wind3.hub>
2024-04-27 11:33:20 +0200

    added overview jupyter notebook

--- app/spark_script.py
+++ app/spark_script.py
@@ -4,31 +4,22 @@ current_dir = os.path.dirname(os.path.abspath(__file__))
 project_root = os.path.abspath(os.path.join(current_dir, '..'))
 sys.path.append(project_root)
 from pyspark.sql import SparkSession
-from decouple import config
-
 
 # Create a Spark session
 spark = SparkSession.builder \
     .appName("Apache iceberg practice") \
+    .config("spark.driver.extraJavaOptions", f"-Dderby.system.home={project_root}/iceberg_tables") \
     .config("spark.jars",
-            f"{project_root}/jars/postgresql-42.7.3.jar,"
             f"{project_root}/jars/iceberg-spark-runtime-3.5_2.12-1.5.1.jar") \
     .config("spark.sql.catalog.spark_catalog", "org.apache.iceberg.spark.SparkSessionCatalog") \
     .config("spark.sql.catalog.spark_catalog.type", "hadoop") \
-    .config("spark.sql.catalog.spark_catalog.warehouse", "/Users/france.cama/code/iceberg-practice/iceberg_tables") \
+    .config("spark.sql.catalog.spark_catalog.warehouse", f"{project_root}/iceberg_tables") \
     .getOrCreate()
 
-url = f"jdbc:postgresql://localhost:5432/{config('DB_NAME')}"
-properties = {
-    "user": config('DB_USER'),
-    "password": config('POSTGRE_PW'),
-    "driver": "org.postgresql.Driver"
-}
-table_name = "onewayflights"
+df = spark.read.csv(f"{project_root}/titanic.csv", header=True, inferSchema=True)
 
-df = spark.read.jdbc(url, table_name, properties=properties)
+# Write to the iceberg table
+df.write.format("iceberg").mode("append").saveAsTable("titanic")
 
-# Write to the table
-df.write.format("iceberg").mode("append").saveAsTable("onewayflights")
 # Print the schema of the DataFrame
 df.printSchema()
